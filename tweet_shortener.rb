8c61dfccc79cf68d556cdd2f2f952c485046ed13
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:47:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,19 +41,3 @@ def bulk_tweet_shortener(tweets)
     puts word_substituter(tweet)
   end
 end
-
-def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
-end
-
-def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + '...'
-  else
-    tweet
-  end
-end
