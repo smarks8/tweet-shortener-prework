bb257d30ec330c56ecdecfa7d0ec88512caec727
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:38:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,6 +14,8 @@ def word_substituter(tweet)
   tweet.split.map do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
+    else 
+      word
     end 
   end.join(" ")
 end 
