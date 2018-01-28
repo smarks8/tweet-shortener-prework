1c9b1847d5d0a5759427241b75223cd6589ac6c5
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:39:18 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,9 @@
 def dictionary 
   dictionary = {
     "hello" => 'hi',
-    "to, two, too" => '2',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2'
     "for, four" => '4',
     'be' => 'b',
     'you' => 'u',
