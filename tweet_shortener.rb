8fb8690cb93a348440a8b0f283bf380fa4679211
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:42:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,6 +23,7 @@ def word_substituter(tweet)
 end 
 
 def bulk_tweet_shortener(array_of_tweets)
-  array_of_tweets.each 
-  
+  array_of_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end 
 end 
