ae035d3b5ee3859aa2354c0339b235cb93c966a4
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:34:56 +0000

    automatically backed up by learn

---
+++ tweet_shortener.rb
@@ -0,0 +1,16 @@
+def dictionary 
+  dictionary = {
+    "hello" => 'hi',
+    "to, two, too" => '2',
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+
+def word_substituter(tweet)
+  tweet.split.map do |word|
+    if dictionary.keys.
+end 
