d48aab86782ef3d5be9d487bcfc0118d3e558ffd
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:47:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,43 +1,30 @@
-
-# Write your code here.
-def dictionary
-dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
-end
-
-# You can use regex or, check out the solution below that
-# def word_substituter(tweet)
-#   tweet = tweet.dup
-#   dictionary.each do |word, replacement|
-#     if tweet.include?(word)
-#       tweet.gsub!(/\b#{word}\b/i, replacement)
-#     end
-#   end
-#   tweet
-# end
-
+def dictionary 
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end 
 
 def word_substituter(tweet)
-  tweet.split.collect do |word|
+  tweet.split.map do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
-    else
+    else 
       word
-    end
+    end 
   end.join(" ")
-end
+end 
 
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
     puts word_substituter(tweet)
   end
-end
+end 
