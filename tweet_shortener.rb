90d7a600270af11896ef1b42b4f11b85f3eb17a4
Stacey  Marks <staceyamarks@gmail.com>
2018-01-28 19:28:29 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,11 @@
 def dictionary 
   {
-    
+    "hello" becomes 'hi',
+    "to, two, too" become '2',
+    "for, four" become '4',
+    'be' becomes 'b',
+    'you' becomes 'u',
+    "at" becomes "@",
+    "and" becomes "&"
   }
 end 
